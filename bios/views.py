df23b8238c3902bd731b3e05fd6a33c170603dfb
Yue Long <yuelong@Yues-MacBook-Pro.local>
2022-04-18 12:12:55 -0400

    removed underscore in names

--- bios/views.py
+++ bios/views.py
@@ -23,11 +23,6 @@ def split(value, key):
     """
     return value.split(key)
 
-@register.filter
-def addstr(arg1, arg2):
-    """concatenate arg1 & arg2"""
-    return str(arg1) + str(arg2)
-
 LOCATION_CHOICES = [
     ('Columbia, MD (HQ)', 'Columbia, MD (HQ)'),
     ('New York, NY', 'New York, NY'),
